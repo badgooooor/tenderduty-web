af4c5c3054588cf3b6ba7c3b7cfa7baab42dce0f
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-02-04 08:57:39 +0700

    :heavy_plus_sign: Add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
   }),
   integrations: [tailwind({
     applyBaseStyles: false
-  }), react()]
+  }), react(), svelte()]
 });
