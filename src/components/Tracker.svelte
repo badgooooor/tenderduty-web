0307a2a99ad88644c8fcf9dda8ca7c5e40d96aa1
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-02-04 09:52:24 +0700

    :lipstick: Add tooltip of tracker item

--- src/components/Tracker.svelte
+++ src/components/Tracker.svelte
@@ -6,6 +6,14 @@
 
 <div class="flex flex-row w-full">
   {#each data as item}
-    <div class={`h-8 w-2 mr-0.5 ${item.color ?? 'bg-black'}`} />
+    <span class={`group relative h-8 w-2 mr-0.5 ${item.color ?? 'bg-black'}`}>
+      <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto z-40">
+        <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+          {item.tooltip}
+          <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" xml:space="preserve"><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+        </div>
+      </div>
+      <span />
+    </span>
   {/each}
 </div>
