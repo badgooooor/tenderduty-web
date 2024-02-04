cc7f1bf2e13f4d128901dc02909ffdec2fa03747
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-02-04 08:59:39 +0700

    :sparkles: Add validator state tracker

---
+++ src/components/Tracker.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { TrackerItem } from '../types';
+
+  export let data: TrackerItem[];
+</script>
+
+<div class="flex flex-row w-full">
+  {#each data as item}
+    <div class={`h-8 w-2 mr-0.5 ${item.color ?? 'bg-black'}`} />
+  {/each}
+</div>
